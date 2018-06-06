b3edc48ec44f61a386494ced05865599a82c3488
Leandro Severino <lseverino@gmail.com>
2018-06-06 18:57:46 -0300

    Fix the incompatible version of rxjs

--- src/frontend/website/src/app/app.component.ts
+++ src/frontend/website/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  clientes = [
+    { nome: 'João'},
+    { nome: 'Mariana'}
+  ];
 }
--- src/frontend/website/src/app/app.module.ts
+++ src/frontend/website/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { TableModule } from 'primeng/table';
+
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
